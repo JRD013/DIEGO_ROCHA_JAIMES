50f69e7b0c675635167d05da218e280126e6563a
Jose Diego Rocha Jaimes <rochadiego013@gmail.com>
2021-06-06 22:11:36 -0400

    Nav Bar

--- testAngular/src/app/app-routing.module.ts
+++ testAngular/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Comp1Component } from './comp1/comp1.component';
+import { Comp2Component } from './comp2/comp2.component';
+import { Comp3Component } from './comp3/comp3.component';
+import { Comp4Component } from './comp4/comp4.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'CIPEC',
+    component: Comp1Component
+  },
+  {
+    path: 'INICIO',
+    component: Comp2Component
+  },
+  {
+    path: 'QUIENES SOMOS',
+    component: Comp3Component
+  },
+  {
+    path: 'DIRECCION',
+    component: Comp4Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
